0c1da81eeff599f904341214edab14f961f3a77e
Nitin Das <nitinhash1308@gmail.com>
2022-07-17 13:49:25 +0530

    :zap: Styled Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,17 @@
 
 import React, { Component } from 'react'
+import logo from "../logo.jpg";
 
 export default class Navbar extends Component {
   render() {
     return (
-        <div style={{display:'flex', background:"white", color:"blue",padding:'1rem', justifyContent:"center", alignItems:"center"}}>
-            <h1>Movies App</h1>
-            <h2 style={{marginLeft:"2rem",}}>Favourites</h2>
+        <div style={{display:'flex', justifyContent:"space-between", background:"", color:"blue",padding:'0.3rem', alignItems:"center"}}>
+            <h1 style={{marginLeft:"5rem",}}><button type="button" className="btn btn-primary">Home</button></h1>
+            <div style={{display:"flex",alignItems:"center"}}>
+              <img style={{width:"4rem",marginRight:"1.5rem"}} src={logo} alt="Logo"/>
+              <h2 className="display4">MOVIES APP</h2>
+            </div>
+            <h2 style={{marginRight:"5rem",}}><button type="button" className="btn btn-dark ">Favourites</button></h2>
       </div>
     )
   }
